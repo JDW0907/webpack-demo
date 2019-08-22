782a7e9a39041b3af2845d0e4979842deb5f227f
JDW0907 <811706959@qq.com>
2019-08-22 13:08:15 +0800

    压缩main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 var path = require('path');
+const UglifyJsPlugin=require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+ //压缩js
+ optimization: {
+  minimizer: [
+      new UglifyJsPlugin({
+          uglifyOptions: {
+              compress: false
+          }
+      })
+  ]
+}
 };
